859103bad6f71f182a943d40f41e79ac4da394de
Paul Goins <general@vultaire.net>
2009-07-21 22:29:25 +0900

    Updated JMdict parser, now organizes data better and has (ugly) string output.

--- parsers/jmdict.py
+++ parsers/jmdict.py
@@ -30,6 +30,18 @@
 
 """A basic parser for JMdict."""
 
+# Need to heavily revise this.
+# We get everything in, but things aren't properly associated.
+#
+# For each Entry:
+#  1 ID
+#  0+ kanji elements (kanji blob, info, priority)
+#  1+ reading elements (reading blob, info, priority)
+#  0/1 info elements (bibliography stuff, etc.)
+#  1+ sense elements (glosses, etc.)
+#
+# 
+
 
 from xml.sax.handler import ContentHandler, DTDHandler, EntityResolver
 from xml.sax.xmlreader import InputSource, XMLReader
@@ -47,9 +59,34 @@ class JMdictEntry(object):
 
     """
 
+    def __init__(self):
+        self.ent_seq = None
+        self.k_ele = []
+        self.r_ele = []
+        self.info = None
+        self.sense = []
+
     def to_string(self, **kwargs):
         """A default "to-string" dump of a JMdictEntry."""
-        return _(u"<%08X:not_yet_implemented>") % id(self)
+
+        s = []
+        s.append(u"JMdictEntry %d" % self.ent_seq)
+        if self.k_ele:
+            s.append(u"Kanji blobs: %s" % u",".join(
+                [elem[u"keb"] for elem in self.k_ele]))
+            s.append(u"k_ele: %s" % unicode(self.k_ele))
+        if self.r_ele:
+            s.append(u"Reading blobs: %s" % u",".join(
+                [elem[u"reb"] for elem in self.r_ele]))
+            s.append(u"r_ele: %s" % unicode(self.r_ele))
+        if self.info: s.append(u"info: %s" % unicode(self.info))
+        if self.sense:
+            if len(self.sense) == 1:
+                s.append(u"Sense: %s" % unicode(self.sense))
+            else:
+                for i, sense in enumerate(self.sense):
+                    s.append(u"Sense %d: %s" % (i+1, unicode(sense)))
+        return u"\n".join(s)
 
 class JMDSAXHandler(ContentHandler):
 
@@ -88,31 +125,89 @@ class JMDSAXHandler(ContentHandler):
                            for k, v in self.path[-1][1].items()])
 
     def startElement(self, name, attrs):
-        if name == "entry":
+        if name == u"entry":
             self.parsing = True
             self.entry = JMdictEntry()
         elif self.parsing:
             self.path.append((name, attrs))
+            if name in (u"k_ele", u"r_ele", u"sense", u"links", u"audit"):
+                # Create a temp var for the current reading, sense, etc.
+                key = u"cur_%s" % name
+                setattr(self.entry, key, {})
+            elif name == u"info":
+                self.entry.info = {}
 
     def endElement(self, name):
+        entry = self.entry
         if self.parsing:
             self.node_content = self.node_content.strip()
             if self.node_content:
                 # Assign data as appropriate
                 node, attrs = self.path[-1]
-                if node == u"ent_seq":   # Unique ID for entry
-                    setattr(self.entry, node, int(self.node_content))
-                elif node in (u"reb", u"keb"):  # Japanese reading/kanji blobs
-                    setattr(self.entry, node, self.node_content)
-                elif node == u"re_nokanji":     # A boolean flag
-                    setattr(self.entry, node, True)
-                else:   # Everything else is a list...
-                    data = getattr(self.entry, node, None)
-                    if data:
-                        data.append(self.node_content)
-                    else:
-                        setattr(self.entry, node, [self.node_content])
+                # Unique ID for entry
+                if node == u"ent_seq":
+                    entry.ent_seq = int(self.node_content)
+                # Kanji elements (blob, info, priority)
+                elif node == u"keb":
+                    entry.cur_k_ele[node] = self.node_content
+                elif node[:3] == u"ke_":
+                    entry.cur_k_ele.setdefault(node, []).append(
+                        self.node_content)
+                # Reading elements (blob, nokanji?, reading substrs, inf, pri)
+                elif node == u"reb":
+                    entry.cur_r_ele[node] = self.node_content
+                elif node == u"re_nokanji": # special case
+                    entry.cur_r_ele[node] = True
+                elif node[:3] == u"re_": # reading element (all but nokanji)
+                    entry.cur_r_ele.setdefault(node, []).append(
+                        self.node_content)
+                # Info element
+                # links [], bibl [], etym [], audit []
+                # links: (tag, desc, uri)
+                # audit: upd_date, upd_detl)
+                # bibl, etym, and all other child fields: strings
+                elif node in (u"bibl", u"etym"):
+                    entry.info.setdefault(node, []).append(
+                        self.node_content)
+                # These info nodes need to be appended on the
+                # endElement event.  *** TO DO ***
+                elif node in (u"link_tag", u"link_desc", u"link_uri",
+                              u"upd_date", u"upd_detl"):
+                    setattr(entry, u"cur_%s" % node, self.node_content)
+                # Sense elements (all but glosses)
+                elif node in (u"stagk", u"stagr", u"pos", u"xref", u"ant",
+                              u"field", u"misc", u"s_inf", u"dial",
+                              u"example"):
+                    entry.cur_sense.setdefault(node, []).append(
+                        self.node_content)
+                elif node == u"lsource":
+                    # xml_lang is common
+                    xml_lang = attrs.get(u"xml:lang", u"eng")
+                    # ls_* seem new...
+                    ls_type = attrs.get(u"ls_type", u"full")
+                    ls_wasei = attrs.get(u"ls_wasei")  # Flag for "waseieigo"
+                    # We'll do a 4 node tuple for this entry...
+                    entry.cur_sense.setdefault(node, []).append(
+                        (self.node_content, xml_lang, ls_type, ls_wasei))
+                # Glosses...  It seems that <pri> is not yet used, so
+                # glosses are pretty straightforward like the above fields.
+                elif node == u"gloss":
+                    xml_lang = attrs.get(u"xml:lang", u"eng")
+                    g_gend = attrs.get(u"g_gend")
+                    entry.cur_sense.setdefault(node, []).append(
+                        (self.node_content, xml_lang, g_gend))
+                elif node == u"pri":
+                    print (u"DEBUG: <pri> field detected!  This is a new "
+                           u"field; please contact the author with the "
+                           u"modification date of your copy of JMdict so he "
+                           u"can update J-Ben to support it!")
+
+                else:   # Unhandled
+                    print (u"DEBUG: path %s: unhandled node %s with content "
+                           u"[%s]" % (self.get_path(), node,
+                                      self.node_content))
                 self.node_content = ""
+
             if self.path:
                 if name != self.path[-1][0]:
                     # Shouldn't ever happen, but mistakes *can* slip in...
@@ -120,8 +215,47 @@ class JMDSAXHandler(ContentHandler):
                           % (self.get_path(), name)
                 else:
                     self.path.pop()
-            if name == "entry":
-                self.entry = None
+
+            # Handle composite values
+            # First, the dict types...
+            if name in (u"k_ele", u"r_ele", u"sense"):
+                temp_key = u"cur_%s" % name
+                obj = getattr(entry, temp_key)
+                getattr(entry, name).append(obj)
+                delattr(entry, temp_key)
+            # Next, the two tuple types
+            elif name == u"links":
+                entry.info.setdefault(u"links", []).append(
+                    (entry.cur_link_tag,
+                     entry.cur_link_desc,
+                     entry.cur_link_uri))
+                delattr(entry, u"cur_link_tag")
+                delattr(entry, u"cur_link_desc")
+                delattr(entry, u"cur_link_uri")
+            elif name == u"audit":
+                entry.info.setdefault(u"audit", []).append(
+                    (entry.cur_upd_date,
+                     entry.cur_upd_detl))
+                delattr(entry, u"cur_upd_date")
+                delattr(entry, u"cur_upd_detl")
+
+            # Handle end of entry
+            elif name == u"entry":
+                for node in (u"k_ele", u"r_ele", u"sense",
+                             u"link_tag", u"link_desc", u"link_uri",
+                             u"upd_date", u"upd_detl"):
+                    if hasattr(entry, u"cur_%s" % node):
+                        print vars(entry)
+                        print node
+                        raise Exception(u"Shouldn't-Happen-Error")
+
+                ent_seq = entry.ent_seq
+                if ent_seq in range(1000090, 2014140):
+                    print entry.to_string(), "\n"
+                if ent_seq >= 1000090:
+                    import sys
+                    sys.exit(-1)
+                entry = None
                 self.parsing = False
 
     def characters(self, content):
