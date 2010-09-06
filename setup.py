1ab8a3224d76e9f3c80c27711671769125277176
unknown <Vultaire@.(none)>
2010-09-06 19:59:14 +0900

    Not sure if PyGTK prereq should be specified here, but doing anyway...

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='jben',
         'jben.interface.gtk.window',
         ],
       package_data={'jben': ['images/*.xpm']},
-      install_requires=['jbparse'],
+      install_requires=['jbparse', 'PyGTK'],
       classifiers=[
           'Environment :: Win32 (MS Windows)',
           'Environment :: X11 Applications :: GTK',
